de75d4b7fc06ca45a7741a8b7086244e6c8f7826
trucnguyen <truc.nguyen@akadigital.vn>
2022-09-27 17:14:06 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyCkMFPKWVfxMkbR5jkwtJJdMlfbS1rX8sk',
+    messagingSenderId: '822761089009',
+    appId: '1:822761089009:web:47f8baf57531f34be10650',
+    projectId: 'akastore-project',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
+    siteid: '898deb41e22a12ae7c2b27e88433e9de'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
