25a7470613015cc7cb4c0c6883e73287a42a63e6
trucnguyen <truc.nguyen@akadigital.vn>
2023-02-17 16:06:14 +0700

    17/2/23 commit

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,13 @@ function openPopup1() {
 }
 
 function closePopup1() {
+    //close popup
     document.getElementById("test1").style.display = "none";
+
+    //stop youtube embed video
+    var ysrc = document.getElementById("iframe1").src;
+    var newsrc = ysrc.replace("&autoplay=1", "");
+    document.getElementById("iframe1").src = newsrc;
 }
 
 function openPopup2() {
@@ -12,6 +18,9 @@ function openPopup2() {
 
 function closePopup2() {
     document.getElementById("test2").style.display = "none";
+    var ysrc = document.getElementById("iframe2").src;
+    var newsrc = ysrc.replace("&autoplay=1", "");
+    document.getElementById("iframe2").src = newsrc;
 }
 
 function openPopup4() {
@@ -20,6 +29,20 @@ function openPopup4() {
 
 function closePopup4() {
     document.getElementById("test4").style.display = "none";
+    var ysrc = document.getElementById("iframe4").src;
+    var newsrc = ysrc.replace("&autoplay=1", "");
+    document.getElementById("iframe4").src = newsrc;
+}
+
+function openPopup5() {
+    document.getElementById('test5').style.display = 'block';
+}
+
+function closePopup5() {
+    document.getElementById("test5").style.display = "none";
+    var ysrc = document.getElementById("iframe5").src;
+    var newsrc = ysrc.replace("&autoplay=1", "");
+    document.getElementById("iframe5").src = newsrc;
 }
 
 function openPopup6() {
@@ -28,4 +51,7 @@ function openPopup6() {
 
 function closePopup6() {
     document.getElementById("test6").style.display = "none";
+    var ysrc = document.getElementById("iframe6").src;
+    var newsrc = ysrc.replace("&autoplay=1", "");
+    document.getElementById("iframe6").src = newsrc;
 }
